b45485602d77d69145be9c3c56b23f024d601f98
haiyinlong <haiyinlong@syriusrobotics.com>
2020-07-04 15:14:11 +0800

    init

---
+++ deploy_my_blog.sh
@@ -0,0 +1,8 @@
+git init
+git add -A
+git commit -m 'init'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:haiwang2517/my-blog.git master
+
